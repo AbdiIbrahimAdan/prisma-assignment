24b87ff73f597f5ba0ee5f28fd90cec46d8c2bf5
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-06-28 08:18:54 +0300

    create prisma and create post to add new product

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import productRouter from './routes/product.routes.js';
+import { config } from 'dotenv';
 const app = express();
+app.use(express.json());
 
 app.use("/product", productRouter);
+config();
 
 app.listen(3005, (req, res)=>{
     console.log("Server running on port 3005.......");
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-
+import {PrismaClient} from '@prisma/client';
 
 const router = Router();
+const prisma = new PrismaClient();
 router.get("/", (req, res) =>{
     res.send("get all the product");
 })
@@ -10,8 +11,22 @@ router.get("/:id", (req, res) =>{
     res.send("get single product");
 })
 
-router.post("/", (req, res) =>{
-    res.send("create a new product");
+router.post("/", async(req, res) =>{
+   try{
+        const{productThumbnail, productTitle, productDescription, productCost, onOffer } = req.body;
+        const newProduct = await prisma.product.create({
+            data: {
+                productThumbnail, 
+                productTitle, 
+                productDescription, 
+                productCost, 
+                onOffer
+            }
+        })
+        res.status(201).json(newProduct);
+    }catch(e){
+        res.status(500).json({success:false, message:e.message})
+    }
 })
 router.patch("/:id", (req, res) =>{
     res.send("update a product");
@@ -23,3 +38,5 @@ router.delete("/:id", (req, res) =>{
 
 
 export default router;
+
+
