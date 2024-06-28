347ad3af8e13862069f38a58de4ebc12f1b1b811
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-06-28 09:49:30 +0300

    hiding the sensitive information

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import {PrismaClient} from '@prisma/client';
 import { getAllProduct, getSingleProduct, createProduct, updatingProduct, deletingProduct } from "../controllers/product.controllers.js";
 
 const router = Router();
-const prisma = new PrismaClient();
 router.get("/", getAllProduct).get("/:id", getSingleProduct).post("/", createProduct).patch("/:id", updatingProduct).delete("/:id", deletingProduct)
 
 
